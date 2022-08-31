74eedf51fbd99b5c21a496766fde95ce1505fb9d
Kevin Carranza <kjcarranzap@gmail.com>
2022-08-31 04:10:35 -0500

    se añade función desistir

---
+++ js/desistir.js
@@ -0,0 +1,12 @@
+var boton_desistir = document.querySelector(".boton_desistir");
+
+boton_desistir.addEventListener("click", function(){
+    document.querySelector("#botones_principales").classList.remove("pagina_agregar_palabra");
+    document.querySelector("#botones_principales").classList.add("pagina_bienvenida");
+    document.querySelector(".ahorcado").style.display = "";
+    document.querySelector(".adivinar").style.display = "";
+    document.querySelector(".boton_iniciar").style.display = "";
+    document.querySelector(".boton_agregar").style.display = "";
+    document.querySelector(".boton_nuevo").style.display = "";
+    document.querySelector(".boton_desistir").style.display = "";
+});
